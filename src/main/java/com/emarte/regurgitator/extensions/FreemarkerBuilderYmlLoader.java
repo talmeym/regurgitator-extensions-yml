edb7f4d982c5062571e9927964616914e7cedb5c
talmeym <mtalmey@gmail.com>
2017-10-28 18:35:56 +0100

    standardised method names in config utils, fixed types in create jms response

--- src/main/java/com/emarte/regurgitator/extensions/FreemarkerBuilderYmlLoader.java
+++ src/main/java/com/emarte/regurgitator/extensions/FreemarkerBuilderYmlLoader.java
@@ -10,7 +10,7 @@ import java.util.Set;
 
 import static com.emarte.regurgitator.core.CoreConfigConstants.*;
 import static com.emarte.regurgitator.core.Log.getLog;
-import static com.emarte.regurgitator.core.YmlConfigUtil.loadOptionalBoolean;
+import static com.emarte.regurgitator.core.YmlConfigUtil.loadOptionalBool;
 import static com.emarte.regurgitator.core.YmlConfigUtil.loadOptionalStr;
 import static com.emarte.regurgitator.extensions.ExtensionsConfigConstants.ALL_CONTEXTS;
 
@@ -22,7 +22,7 @@ public class FreemarkerBuilderYmlLoader extends FreemarkerBuilderLoader implemen
         String source = loadOptionalStr(yaml, SOURCE);
         String value = loadOptionalStr(yaml, VALUE);
         String file = loadOptionalStr(yaml, FILE);
-        boolean allContexts = loadOptionalBoolean(yaml, ALL_CONTEXTS);
+        boolean allContexts = loadOptionalBool(yaml, ALL_CONTEXTS);
         return buildFreemarkerValueBuilder(source, value, file, allContexts, log);
     }
 }
--- src/main/java/com/emarte/regurgitator/extensions/VelocityBuilderYmlLoader.java
+++ src/main/java/com/emarte/regurgitator/extensions/VelocityBuilderYmlLoader.java
@@ -10,7 +10,7 @@ import java.util.Set;
 
 import static com.emarte.regurgitator.core.CoreConfigConstants.*;
 import static com.emarte.regurgitator.core.Log.getLog;
-import static com.emarte.regurgitator.core.YmlConfigUtil.loadOptionalBoolean;
+import static com.emarte.regurgitator.core.YmlConfigUtil.loadOptionalBool;
 import static com.emarte.regurgitator.core.YmlConfigUtil.loadOptionalStr;
 import static com.emarte.regurgitator.extensions.ExtensionsConfigConstants.ALL_CONTEXTS;
 
@@ -22,7 +22,7 @@ public class VelocityBuilderYmlLoader extends VelocityBuilderLoader implements Y
         String source = loadOptionalStr(yaml, SOURCE);
         String value = loadOptionalStr(yaml, VALUE);
         String file = loadOptionalStr(yaml, FILE);
-        boolean allContexts = loadOptionalBoolean(yaml, ALL_CONTEXTS);
+        boolean allContexts = loadOptionalBool(yaml, ALL_CONTEXTS);
         return buildVelocityValueBuilder(source, value, file, allContexts, log);
     }
 }
